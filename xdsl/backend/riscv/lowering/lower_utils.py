d89d1bb1de991e3c5fc5edcd89d64e870cbbce8c
Alban Dutilleul <alban.dutilleul@ens-rennes.fr>
2023-07-20 10:14:13 +0100

    don't use cast with multiple operands

--- xdsl/backend/riscv/lowering/lower_utils.py
+++ xdsl/backend/riscv/lowering/lower_utils.py
@@ -9,23 +9,20 @@ from xdsl.pattern_rewriter import PatternRewriter
 def cast_values_to_registers(
     operands: Sequence[SSAValue], rewriter: PatternRewriter
 ) -> list[OpResult]:
-    if not operands:
-        return []
-
-    types: list[Attribute] = []
+    results: list[OpResult] = []
     for op in operands:
+        cast_type = riscv.RegisterType(riscv.Register())
         if isinstance(op.type, AnyFloat):
-            types.append(riscv.FloatRegisterType(riscv.Register()))
+            cast_type = riscv.FloatRegisterType(riscv.Register())
         elif isinstance(op.type, builtin.IntegerType | builtin.IndexType):
-            types.append(riscv.RegisterType(riscv.Register()))
+            cast_type = riscv.RegisterType(riscv.Register())
         elif isinstance(op.type, riscv.RegisterType | riscv.FloatRegisterType):
-            types.append(op.type)
-        else:
-            types.append(riscv.RegisterType(riscv.Register()))
+            cast_type = op.type
 
-    cast = builtin.UnrealizedConversionCastOp.get(operands, types)
-    rewriter.insert_op_before_matched_op(cast)
-    return cast.results
+        cast = builtin.UnrealizedConversionCastOp.get([op], (cast_type,))
+        results.append(cast.results[0])
+        rewriter.insert_op_before_matched_op(cast)
+    return results
 
 
 def get_type_size(t: Attribute) -> int:
