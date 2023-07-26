3f9fbcf0891c35b313a8f93b3ddf460e2720987b
Anton Lydike <me@antonlydike.de>
2023-07-26 16:51:24 +0100

    add riscv_scf to xdsl opt

--- xdsl/tools/command_line_tool.py
+++ xdsl/tools/command_line_tool.py
@@ -35,6 +35,7 @@ from xdsl.dialects.test import Test
 from xdsl.dialects.vector import Vector
 from xdsl.frontend.passes.desymref import DesymrefyPass
 from xdsl.frontend.symref import Symref
+from xdsl.dialects.riscv_scf import RISCV_Scf
 from xdsl.ir import Dialect, MLContext
 from xdsl.parser import Parser
 from xdsl.passes import ModulePass
