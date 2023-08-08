fbf67d3e9445a09078ee28e3ff1520d9ad9e240a
Alban Dutilleul <alban.dutilleul@ens-rennes.fr>
2023-08-08 12:02:27 +0100

    move lowering to experimental folder

--- xdsl/tools/command_line_tool.py
+++ xdsl/tools/command_line_tool.py
@@ -3,7 +3,7 @@ import os
 import sys
 from typing import IO, Callable
 
-from xdsl.backend.riscv import riscv_scf_to_asm
+from xdsl.backend.riscv.experimental import riscv_scf_to_asm
 from xdsl.backend.riscv.lowering import scf_to_riscv_scf
 from xdsl.backend.riscv.lowering.lower_func_riscv_func import LowerFuncToRiscvFunc
 from xdsl.backend.riscv.lowering.optimisation_riscv import OptimiseRiscvPass
@@ -50,7 +50,6 @@ from xdsl.transforms import (
     printf_to_llvm,
     reconcile_unrealized_casts,
     riscv_register_allocation,
-    riscv_scf_to_asm,
 )
 from xdsl.transforms.experimental import (
     convert_stencil_to_ll_mlir,
@@ -118,7 +117,6 @@ def get_all_passes() -> list[type[ModulePass]]:
         stencil_shape_inference.StencilShapeInferencePass,
         stencil_storage_materialization.StencilStorageMaterializationPass,
         reconcile_unrealized_casts.ReconcileUnrealizedCastsPass,
-        riscv_scf_to_asm.LowerScfForToLabels,
     ]
 
 
